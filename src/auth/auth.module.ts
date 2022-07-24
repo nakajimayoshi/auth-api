ff597661bf76aced136729577883be7ef993f42c
Hypeism <jhnwllms236@gmail.com>
2022-07-24 13:05:00 -0700

    updated typescript dependency to 4.7.4, added jwt/passport dependencies and types

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {PrismaModule} from "../prisma-module/prisma.module";
 
 @Module({
     controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,7 +39,7 @@ export class AuthService {
     async signin(dto: AuthDto) {
 
         // find the user by email
-        const user = await this.prisma.user.findFirst({
+        const user = await this.prisma.user.findUnique({
             where: {
                 email: dto.email,
             },
